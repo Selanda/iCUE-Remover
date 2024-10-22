13db07b8ca219a0cbac3a0f8789f1c552c749bed
Selanda <64166375+Selanda@users.noreply.github.com>
2024-10-22 11:18:02 -0400

    Update iCUE_Remover.bat

--- iCUE_Remover.bat
+++ iCUE_Remover.bat
@@ -6,19 +6,19 @@ taskkill /f /im iCUEDevicePluginHost.exe
 taskkill /f /im iCUEUpdateService.exe
 taskkill /f /im Corsair.Service.exe
 taskkill /f /im CpuIdRemote64.exe
-echo •••
+echo ---
 echo This is where you should run the unisntaller from Add/Remove Programs (appwiz.cpl)
-echo •••
+echo ---
 echo Once the uninstaller is complete, DO NOT REBOOT.
 echo Leave the uninstaller application open and press any key to continue.
-echo •••
+echo ---
 pause
 rmdir /S /Q %appdata%\Corsair
 rmdir /S /Q %localappdata%\Corsair
 rmdir /S /Q  "C:\Program Files (x86)\Corsair"
 rmdir /S /Q %programfiles%\Corsair
 rmdir /S /Q %programdata%\Corsair
-echo •••
+echo ---
 echo Leftover folders have been removed. You may now complete the reboot process prompted by the uninstaller.
-echo •••
+echo ---
 pause
